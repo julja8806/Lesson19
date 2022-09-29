a01b1599fcc0f4a5703740352744f4981f3879aa
julja8806 <julja8806@mail.ru>
2022-09-29 23:42:50 +0300

    max, min, avg, geometricAvg

--- src/by/itstep/julja8806/model/ArrayWorker.java
+++ src/by/itstep/julja8806/model/ArrayWorker.java
@@ -2,7 +2,7 @@ package by.itstep.julja8806.model;
 
 public class ArrayWorker {
     public static int max(int[] array) {
-        if ( array == null || array.length == 0) {
+        if (array == null || array.length == 0) {
             throw new RuntimeException();
         }
         int max = array[0];
@@ -14,4 +14,40 @@ public class ArrayWorker {
         return max;
     }
 
+    public static int min(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new RuntimeException();
+        }
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+    public static double avg(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new RuntimeException();
+        }
+        double s = 0;
+        for (int i = 0; i < array.length; i++) {
+            s += array[i];
+        }
+
+        return s / array.length;
+
+    }
+    public static double geometricAvg(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new RuntimeException();
+        }
+        double s = 1;
+        for (int i = 0; i < array.length; i++) {
+            s *= array[i];
+        }
+
+        return Math.cbrt(s);
+
+    }
 }
--- test/by/itstep/julja8806/model/logic/ArrayWorkerTest.java
+++ test/by/itstep/julja8806/model/logic/ArrayWorkerTest.java
@@ -2,6 +2,8 @@ package by.itstep.julja8806.model.logic;
 
 import by.itstep.julja8806.model.ArrayWorker;
 import org.junit.Test;
+
+
 import static org.junit.Assert.*;
 
 public class ArrayWorkerTest {
@@ -24,6 +26,61 @@ public class ArrayWorkerTest {
          ArrayWorker.max(null);
 
     }
+    @Test
+    public void testMinPositive(){
+        int [] array = {2, 5, 8, 7, 6};
+        int expected = 2;
+        int actual = ArrayWorker.min(array);
+        assertEquals(expected, actual);
+    }
+    @Test(expected = RuntimeException.class)
+    public void testMinWithEmptyObject() {
+        int[] array = {};
 
+        ArrayWorker.max(array);
+    }
+    @Test(expected = RuntimeException.class)
+    public void testMinWithNull(){
 
+        ArrayWorker.max(null);
+
+    }
+    @Test
+    public void testAveragePositive(){
+        int [] array = {2, 5, 8, 7, 6};
+        double expected = 5.60;
+        double actual = ArrayWorker.avg(array);
+        assertEquals(expected, actual, 0.001);
+    }
+    @Test(expected = RuntimeException.class)
+    public void testAverageWithEmptyObject() {
+        int[] array = {};
+
+        ArrayWorker.avg(array);
+    }
+    @Test(expected = RuntimeException.class)
+    public void testAverageWithNull(){
+
+        ArrayWorker.avg(null);
+
+    }
+    @Test
+    public void testGeometricAvgPositive(){
+        int [] array = {2, 5, 8};
+        double expected = 4.30;
+        double actual = ArrayWorker.geometricAvg(array);
+        assertEquals(expected, actual, 0.001);
+    }
+    @Test(expected = RuntimeException.class)
+    public void testGeometricAvgWithEmptyObject() {
+        int[] array = {};
+
+        ArrayWorker.geometricAvg(array);
+    }
+    @Test(expected = RuntimeException.class)
+    public void testGeometricAvgWithNull(){
+
+        ArrayWorker.geometricAvg(null);
+
+    }
 }
